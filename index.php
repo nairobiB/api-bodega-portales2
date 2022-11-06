51a8a66ff5a0868656079e537c9f4843ed827d98
jpaolam <jp_martelg@unicah.edu>
2022-11-06 05:49:10 +0000

    Elimnacion de comentarios

--- index.php
+++ index.php
@@ -5,5 +5,3 @@ require_once('configuraciones/modeloBase.php');
 require_once('configuraciones/vistaBase.php');
 require_once('configuraciones/app.php');
 $app = new App();
-//cambio en el index en rama de dev a
-//jessie
